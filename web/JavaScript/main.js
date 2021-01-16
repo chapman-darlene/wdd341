bf9b1da8815e02055b3c1f71fc80d39af7520833
chapman-darlene <31857968+chapman-darlene@users.noreply.github.com>
2021-01-16 18:19:54 -0500

    major update 01/16/2021 6:19PM

---
+++ web/JavaScript/main.js
@@ -0,0 +1,29 @@
+//main js
+
+
+$('.img-focus').hover(
+    function () {
+    $('.jupiterInfo').show();
+  }, 
+    function () {
+    $('.jupiterInfo').hide();
+  }
+);
+
+// function getPlanet() {
+//     var x = document.getElementById("imgJupiter");
+
+//     if (x == )
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
+
+/*In the future this function will involve a switch statement
+    it grabs the planet id
+    runs it through the switch
+    and prints out the information related to that planet in col-2*/
+
+
--- web/index.php
+++ web/index.php
@@ -4,32 +4,55 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main_style.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <!--phpinfo();-->
   </head>
 
   <body>
 
-    <header>
+    <header>    
         <?php
           include_once 'includes/nav.html';
           ?>
+          <div class="description"><h1>A Few of My Favorite Things</h1></div>
     </header>
     <section class="main-section">
-
-      <div>
-        <h1>Astronomy</h1>
-      </div>
-        <div class="img-selection">
-            <img src="assets/images/venus.jpg">
-            <img src="assets/images/moon.jpg">
-            <img src="assets/images/mars.jpg">
-            <img src="assets/images/jupiter.jpg">
-        </div>
+            
+      <div class="container-fluid"> 
+        <div class="row">
+            <div class="col-md">
+                <h5>Family</h5>
+                <img class="photo" src="assets/images/AdobeStock_292115734_lr.png">
+                <div class="info">
+                  <div class="text"><blockquote cite="https://www.churchofjesuschrist.org/study/manual/gospel-topics/family?lang=eng">
+                  "Whatever problems your family is facing, whatever you must do to solve them, the beginning and the end of the solution is charity, the pure love of Christ."<br>President Dieter F. Uchtdorf
+                  </blockquote></div><!--end blockquote-->
+                </div><!--end info-->
+            </div><!--end col-->
+            <div class="col-md">
+                <h3>Photography</h3>
+                <img class="photo" src="assets/images/AdobeStock_101026418_lr.png">
+                <div class="info">
+                  <div class="text"><blockquote cite="https://www.brainyquote.com/quotes/jacqueshenri_lartigue_141222">
+                  "Photography to me is catching a moment which is passing, and which is true."<br>Jacques-Henri Lartigue
+                  </blockquote></div><!--end block quote-->
+                </div><!--end info-->
+              </div><!--end col-->
+            <div class="col-md">
+                <h3>Astronomy</h3>
+                <img class="photo" src="assets/images/AdobeStock_366936856_lr.png">  
+                <div class="info">
+                  <div class="text"><blockquote cite="https://www.nasa.gov/feature/goddard/2020/nasa-ibex-charts-11-years-change-at-boundary-interstellar-space-heliosphere-sun">
+                  "Far, far beyond the orbits of the planets lie the hazy outlines of the magnetic bubble in space that we call home."<br>Lina Tran, NASA’s Goddard Space Flight Center, Greenbelt, Md.
+                  </blockquote></div><!--end block quote-->
+                </div><!--end info-->   
+            </div><!--end col-->
+        </div><!--end row-->      
+      </div><!--end container-fluid-->
     </section>
-    </body>
+  </body>
 
     <script src="JavaScript/jquery-3.5.1.min.js"></script>
     <script src="JavaScript/jquery-3.5.1.js"></script>
