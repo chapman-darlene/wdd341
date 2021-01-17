e28d1205516927808d17de6941106063f9a985b7
chapman-darlene <31857968+chapman-darlene@users.noreply.github.com>
2021-01-16 20:44:44 -0500

    week2

--- web/assignments/week2/week2.php
+++ web/assignments/week2/week2.php
@@ -4,9 +4,9 @@
  <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Week 2 Group Assignment</title>
-<link href="style.css">
-<link href="/css/main_style.css">
-<link href="../../css/bootstrap.css">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet"href="/css/main_style.css">
+<link rel="stylesheet"href="../../css/bootstrap.css">
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
 </head>
