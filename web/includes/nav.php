5db7dc6cecdfcdc329b63858e2a7f42ea6ead303
chapman-darlene <31857968+chapman-darlene@users.noreply.github.com>
2021-01-16 20:42:05 -0500

    nav

--- web/includes/nav.php
+++ web/includes/nav.php
@@ -1,11 +1,8 @@
 <!-- <head>
     <link rel="stylesheet" src="/css/main_style.css">
 </head> -->
-
-<header class="main_nav">
-    <ul>
+    <ul class="main_nav">
         <li><a href="/index.php">Home</a></li>
         <li><a href="/assignments/week2/week2.php">Weekly Assignments</a></li>
         <li><a href="/project.php">Project</a></li>
     </ul>
-</header>
