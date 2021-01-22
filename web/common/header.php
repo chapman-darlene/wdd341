a3cabc577190396fb3b3fe8ddcbf9423b1340ae7
chapman-darlene <31857968+chapman-darlene@users.noreply.github.com>
2021-01-22 16:05:54 -0500

    css

--- web/common/header.php
+++ web/common/header.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
-    <link rel="stylesheet" href="/web/css/main_style.css">
-    <link href="/web/css/main_nav.css" rel="stylesheet">
+    <link rel="stylesheet" href="/css/main_style.css">
+    <link href="/css/main_nav.css" rel="stylesheet">
     <!-- <link rel="stylesheet" href="../css/bootstrap.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> -->
   
