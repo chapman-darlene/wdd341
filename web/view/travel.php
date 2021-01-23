f8126ac3253c60a43bcc4cd93338099891d52024
chapman-darlene <31857968+chapman-darlene@users.noreply.github.com>
2021-01-23 18:23:33 -0500

    shopping

--- web/view/travel.php
+++ web/view/travel.php
@@ -29,15 +29,17 @@ if (! isset ( $_SESSION ['cart'] )) {
             <img src="../assets/images/jupiter.png" alt="Picture of Jupiter" class="img-cart">
             <h1>Trip to Jupiter</h1>
             <p class="price">$1,500,000*<br> per person</p>
-            <p class="description">Enjoy a luxury trip orbiting Jupiter, with a stay in the elite resort hotel on Mars, for 7 days and 6 nights before returning to Earth.</p>
+            <p class="description">Enjoy a luxury trip orbiting Jupiter, with a stay in the elite resort hotel on Mars, for 7 days and 6 nights before returning to Earth.</p>            
+            <p><input type="number" value="0" hidden></p>
             <p><input type="submit" class="card-button" value="0" name="buy">Add to Cart</p>
         </div> <!--end card -->
+    </form>
         <div class="card">
             <img src="../assets/images/venus.png" alt="Picture of Venus" class="img-cart">
             <h1>Trip to Venus</h1>
             <p class="price">$800,000*<br>per person</p>
             <p class="description">Enjoy a luxury trip orbiting Venus. Stay in the elite resort hotel on the Moon for 4 days and 3 nights before returning to Earth</p>
-            <p><input type="submit" class="card-button" value="1" name="buy">Add to Cart</p>
+            <p><input type="submit" class="card-button" value="Add to Cart" name="buy">Add to Cart</p>
         </div> <!--end card -->
     
         <div class="card">
@@ -45,7 +47,7 @@ if (! isset ( $_SESSION ['cart'] )) {
             <h1>Trip to Mars</h1>
             <p class="price">$900,000*<br>per person</p>
             <p class="description">Enjoy a luxury trip to Mars. Stay in the elite resort hotel on Mars for 7 days and 6 nights before returning to Earth.</p>
-            <p><input type="submit" class="card-button" value="3" name="buy">Add to Cart</p>
+            <p><input type="submit" class="card-button" value="Add to Cart" name="buy">Add to Cart</p>
         </div> <!--end card -->
         <form action="cart.php" method="POST">
         <div class="card">
@@ -53,7 +55,7 @@ if (! isset ( $_SESSION ['cart'] )) {
             <h1>Trip to Saturn</h1>
             <p class="price">$2,000,000*<br>per person</p>
             <p class="description">Enjoy a luxury trip orbiting Saturn, with a stay in the elite resort hotel on Mars, for 7 days and 6 nights before returning to Earth.</p>
-            <p><input type="submit" class="card-button" value ="4" name="buy">Add to Cart</p>
+            <p><input type="submit" class="card-button" value ="Add to Cart" name="buy">Add to Cart</p>
         </div> <!--end card -->
         </form>
 
