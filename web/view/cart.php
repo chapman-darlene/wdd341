b8d3ffa27818696624fd3e525fa1a515d6a9f5ee
chapman-darlene <31857968+chapman-darlene@users.noreply.github.com>
2021-01-23 18:01:09 -0500

    cart

--- web/view/cart.php
+++ web/view/cart.php
@@ -1,11 +1,43 @@
 <?php
-    session_start();
-    $cart = $_SESSION['cart'];
 
-
-
-    echo $cart;
-
+session_start();
+      $itemName;
+      $price;
+      $itemNumber;
+      $ticket = $_POST['ticket[]'];
+      foreach($ticket as $selected){
+  
+          if ($selected == 0) {
+              $ticket = array(
+                  $itemName = 'jupiter',
+                  $price = 1750000,
+                  $itemNumber = 1
+              );
+          }elseif($selected == 1) {
+              $ticket = array(
+                  $itemName = 'venus',
+                  $price = 800000,
+                  $itemNumber = 1
+              );
+          }elseif($selected == 2) {
+              $ticket = array(
+                  $itemName = 'mars',
+                  $price = 900000,
+                  $itemNumber = 1
+              );
+          }elseif($selected == 3) {
+              $ticket = array(
+                  $itemName = 'saturn',
+                  $price = 2000000,
+                  $itemNumber = 1
+              );
+          }
+      }//end foreach
+      $buy = $_POST["buy"];
+      echo $buy;
+      echo $ticket;
+  
+     
     
 
 ?>
--- web/view/travel.php
+++ web/view/travel.php
@@ -1,42 +1,7 @@
 <?php
     include $_SERVER['DOCUMENT_ROOT'] . '/common/header.php';
 
-    $itemName;
-    $price;
-    $itemNumber;
-    $ticket = array();
-    foreach($ticket as $selected){
-
-        if ($selected == 0) {
-            $ticket = array(
-                $itemName = 'jupiter',
-                $price = 1750000,
-                $itemNumber = 1
-            );
-        }elseif($selected == 1) {
-            $ticket = array(
-                $itemName = 'venus',
-                $price = 800000,
-                $itemNumber = 1
-            );
-        }elseif($selected == 2) {
-            $ticket = array(
-                $itemName = 'mars',
-                $price = 900000,
-                $itemNumber = 1
-            );
-        }elseif($selected == 3) {
-            $ticket = array(
-                $itemName = 'saturn',
-                $price = 2000000,
-                $itemNumber = 1
-            );
-        }
-    }//end foreach
-    $buy = $_POST["buy"];
-    print $buy;
-
-    session_start();
+  
     $cart = $_SESSION['cart'];
 
 
@@ -59,7 +24,7 @@ if (! isset ( $_SESSION ['cart'] )) {
 <main>
     <h1>Travel Packages</h1>
     <div class="container">
-    <form action="$_SESSION['PHP_SELF']" method="POST">
+    <form class="main-form" action="cart.php" method="POST">
         <div class="card">
             <img src="../assets/images/jupiter.png" alt="Picture of Jupiter" class="img-cart">
             <h1>Trip to Jupiter</h1>
@@ -68,7 +33,6 @@ if (! isset ( $_SESSION ['cart'] )) {
             <input type="hidden" name="ticket[]">
             <p><input type="submit" class="btn" value="Add to Cart" name="buy"></p>
         </div> <!--end card -->
-
         <div class="card">
             <img src="../assets/images/venus.png" alt="Picture of Venus" class="img-cart">
             <h1>Trip to Venus</h1>
@@ -78,7 +42,7 @@ if (! isset ( $_SESSION ['cart'] )) {
             <input type="hidden" name="ticket[]">
             <p><input type="submit" class="btn" value="Add to Cart" name="buy"></p>
         </div> <!--end card -->
-
+    
         <div class="card">
             <img src="../assets/images/mars.png" alt="Picture of Mars" class="img-cart">
             <h1>Trip to Mars</h1>
