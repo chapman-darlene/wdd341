2b0f24b32e3ce0b339455f1017fc72dcdd3e6297
chapman-darlene <31857968+chapman-darlene@users.noreply.github.com>
2021-01-23 18:36:08 -0500

    shopping

--- web/view/cart.php
+++ web/view/cart.php
@@ -1,6 +1,8 @@
 <?php
 
 session_start();
+$cart = $_SESSION['cart'];
+
       $itemName;
       $price;
       $itemNumber;
--- web/view/travel.php
+++ web/view/travel.php
@@ -4,7 +4,6 @@
     session_start();
     $cart = $_SESSION['cart'];
 
-
 // set Session
 if (! isset ( $_SESSION ['cart'] )) {
     $_SESSION ['cart'] = array($ticket);
@@ -12,6 +11,7 @@ if (! isset ( $_SESSION ['cart'] )) {
 
 // Add
     if (isset ( $_POST ["buy"] )) {
+        $ticket = $_POST['ticket[]'];
         // Check the item is not already in the cart
         if (!in_array($_POST ["buy"], $_SESSION['cart'])) {
             // Add new item to cart
@@ -29,17 +29,17 @@ if (! isset ( $_SESSION ['cart'] )) {
             <img src="../assets/images/jupiter.png" alt="Picture of Jupiter" class="img-cart">
             <h1>Trip to Jupiter</h1>
             <p class="price">$1,500,000*<br> per person</p>
-            <p class="description">Enjoy a luxury trip orbiting Jupiter, with a stay in the elite resort hotel on Mars, for 7 days and 6 nights before returning to Earth.</p>            
-            <p><input type="number" value="0" hidden></p>
-            <p><input type="submit" class="card-button" value="0" name="buy">Add to Cart</p>
+            <p class="description">Enjoy a luxury trip orbiting Jupiter, with a stay in the elite resort hotel on Mars, for 7 days and 6 nights before returning to Earth.</p>this flickering            
+            <p><input type="number" name="ticket[]" value="0" hidden></p>
+            <p><input type="submit" class="card-button" name="buy" value="Add to Cart"></p>
         </div> <!--end card -->
-    </form>
         <div class="card">
             <img src="../assets/images/venus.png" alt="Picture of Venus" class="img-cart">
             <h1>Trip to Venus</h1>
             <p class="price">$800,000*<br>per person</p>
             <p class="description">Enjoy a luxury trip orbiting Venus. Stay in the elite resort hotel on the Moon for 4 days and 3 nights before returning to Earth</p>
-            <p><input type="submit" class="card-button" value="Add to Cart" name="buy">Add to Cart</p>
+            <p><input type="number" name="ticket[]" value="1" hidden></p>
+            <p><input type="submit" class="card-button" name="buy" value="Add to Cart"></p>
         </div> <!--end card -->
     
         <div class="card">
@@ -47,7 +47,8 @@ if (! isset ( $_SESSION ['cart'] )) {
             <h1>Trip to Mars</h1>
             <p class="price">$900,000*<br>per person</p>
             <p class="description">Enjoy a luxury trip to Mars. Stay in the elite resort hotel on Mars for 7 days and 6 nights before returning to Earth.</p>
-            <p><input type="submit" class="card-button" value="Add to Cart" name="buy">Add to Cart</p>
+            <p><input type="number" name="ticket[]" value="2" hidden></p>
+            <p><input type="submit" class="card-button" name="buy" value="Add to Cart"></p>
         </div> <!--end card -->
         <form action="cart.php" method="POST">
         <div class="card">
@@ -55,7 +56,8 @@ if (! isset ( $_SESSION ['cart'] )) {
             <h1>Trip to Saturn</h1>
             <p class="price">$2,000,000*<br>per person</p>
             <p class="description">Enjoy a luxury trip orbiting Saturn, with a stay in the elite resort hotel on Mars, for 7 days and 6 nights before returning to Earth.</p>
-            <p><input type="submit" class="card-button" value ="Add to Cart" name="buy">Add to Cart</p>
+            <p><input type="number" name="ticket[]" value="3" hidden></p>
+            <p><input type="submit" class="card-button" name="buy" value="Add to Cart"></p>
         </div> <!--end card -->
         </form>
 
