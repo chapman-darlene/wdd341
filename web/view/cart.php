da834f06abb822f0fe052004b487addc6fa7705e
chapman-darlene <31857968+chapman-darlene@users.noreply.github.com>
2021-01-28 17:56:41 -0500

    cart

--- web/view/cart.php
+++ web/view/cart.php
@@ -5,8 +5,6 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/common/header.php';
 session_start();
 
 
-$_SESSION['cart'] = $_POST;
-
 if (!isset($_SESSION['cart'])) {   
     $_SESSION['cart'] = array();
     array_push($_SESSION['cart'] = $_POST);
@@ -32,7 +30,7 @@ print_r($_SESSION['cart']);
             <h3 class="cart-h3"><?php echo "Planet: " . $planet; ?></h3><br>
             <h3 class="cart-h3"><?php echo "Price: $" . $price; ?></h3>
             <br>
-            <a href="travel.php"><button  class="btn table-btn" >Continue Shopping</button></a>
+            <a href="travel.php"><button  class="btn" >Continue Shopping</button></a>
         </div>
     </div>
 </main>
