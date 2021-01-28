87d094eb5136ea64eb7d03e806d603e17eda4e58
chapman-darlene <31857968+chapman-darlene@users.noreply.github.com>
2021-01-28 18:00:06 -0500

    cart

--- web/view/cart.php
+++ web/view/cart.php
@@ -23,15 +23,15 @@ print_r($_SESSION['cart']);
 
 <main>
     
-    <div class="container">
-        <div class="cart-display">
+    <div class="cart-container">
+        
             <h1 class="cart-h1">Your Ticket Purchase:</h1><br>
             <br>
             <h3 class="cart-h3"><?php echo "Planet: " . $planet; ?></h3><br>
             <h3 class="cart-h3"><?php echo "Price: $" . $price; ?></h3>
             <br>
             <a href="travel.php"><button  class="btn" >Continue Shopping</button></a>
-        </div>
+       
     </div>
 </main>
 </body>
