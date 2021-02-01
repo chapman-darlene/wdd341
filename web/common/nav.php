0ec8a806f4812c24164fbfec47b711ba338b50d9
chapman-darlene <31857968+chapman-darlene@users.noreply.github.com>
2021-02-01 15:14:42 -0500

    update nav

--- web/common/nav.php
+++ web/common/nav.php
@@ -2,7 +2,7 @@
     echo '<div class="nav">'.
             '<ul>'.
                 '<li><a href="../view/home.php">Home</a></li>'.
-                '<li><a href="../assignments/week2">Assignments</a></li>'.
+                '<li><a href="../assignments/week5">Assignments</a></li>'.
                 '<li><a href="../view/project.php">Project</a></li>'.
             '</ul>'.
         '</div>';
