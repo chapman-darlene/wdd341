495036d397594ae52877a8d0b46ccd384e34767b
chapman-darlene <31857968+chapman-darlene@users.noreply.github.com>
2021-02-02 16:42:56 -0500

    nav

--- web/common/nav.php
+++ web/common/nav.php
@@ -1,18 +1,48 @@
+<?php echo '<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <!-- Brand -->
+  <a class="navbar-brand" href="#">WDD341</a>
+
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="index.php">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="../view/project.php">Project</a>
+    </li>
+
+    <!-- Dropdown -->
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+        Assignments
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="../assignments/week1.html">Week 1</a>
+        <a class="dropdown-item" href="../assignments/week2/week2.php">Week 2</a>
+        <a class="dropdown-item" href="#/">Week 3</a>
+        <a class="dropdown-item" href="../assignments/week5/index.php">Week 5</a>
+      </div>
+    </li>
+  </ul>
+</nav>'
+?>
+
+
 <?php
-    echo '<div class="navbar">
-        <a href="../view/home.php">Home</a>
-        <a href="../view/project.php">Project</a>
-        <div class="dropdown">
-            <button class="dropbtn">Dropdown 
-                <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="dropdown-content">
-                <a href="../assignments/week1">Week 1</a>
-                <a href="../assignments/week2">Week 2</a>
-                <a href="../assignments/week3">Week 3</a>
-                <a href="../assignments/week4">Week 4</a>
-                <a href="../assignments/week5">Week 5</a>
-            </div>
-        </div> 
-    </div>'
+    //echo '<div class="navbar">
+    //     <a href="../view/home.php">Home</a>
+    //     <a href="../view/project.php">Project</a>
+    //     <div class="dropdown">
+    //         <button class="dropbtn">Dropdown 
+    //             <i class="fa fa-caret-down"></i>
+    //         </button>
+    //         <div class="dropdown-content">
+    //             <a href="../assignments/week1">Week 1</a>
+    //             <a href="../assignments/week2">Week 2</a>
+    //             <a href="../assignments/week3">Week 3</a>
+    //             <a href="../assignments/week4">Week 4</a>
+    //             <a href="../assignments/week5">Week 5</a>
+    //         </div>
+    //     </div> 
+    // </div>'
 ?>
